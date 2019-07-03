94bb48fa19457687af8109a3b747c718968d4587
Mikhail Maslyuk <leakymee@bu.edu>
2019-07-03 15:09:45 -0400

    Add example github auth response model for reference

---
+++ models/octokitCreateAuthorizationResponse.js
@@ -0,0 +1,18 @@
+const example = { 
+    id: 305037149,
+    url: 'https://api.github.com/authorizations/305037149',
+    app: {
+        name:'ginits, the command-line tool for initializing Git Repositories',
+        url: 'https://developer.github.com/v3/oauth_authorizations/',
+        client_id: '00000000000000000000' 
+    },
+    token: '[TOKEN_HERE]',
+    hashed_token:'[HASH HERE]',
+    token_last_eight: '[LAST8HERE]',
+    note: 'ginits, the command-line tool for initializing Git Repositories',
+    note_url: null,
+    created_at: '2019-07-03T19:01:28Z',
+    updated_at: '2019-07-03T19:01:28Z',
+    scopes: [ 'user', 'public_repo', 'repo', 'repo:status' ],
+    fingerprint: null
+}
